abc6e8bbe3f671c70581ba1df4a77bc4945216c9
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-01-15 22:38:47 -0800

    feat: add title to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_090230) do
+ActiveRecord::Schema.define(version: 2022_01_16_063656) do
 
   create_table "posts", force: :cascade do |t|
     t.string "author"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
 end
