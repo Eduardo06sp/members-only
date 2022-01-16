fed62baef7595db5d8de50c351e184e9b27ab5b8
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-01-15 23:16:21 -0800

    fix: rollback database for use of Devise User model instead

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_16_065743) do
+ActiveRecord::Schema.define(version: 2022_01_16_063656) do
 
   create_table "posts", force: :cascade do |t|
     t.string "author"
@@ -18,14 +18,6 @@ ActiveRecord::Schema.define(version: 2022_01_16_065743) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
