36a1bc0fd71e0ba83750a47c9fac6939ab4cc4ed
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-01-16 17:38:35 -0800

    feat: assign @post and redirecting/rendering to create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,5 +10,12 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
