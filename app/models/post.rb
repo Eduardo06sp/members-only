d678d9bd489323642f0154d823e770000bf45e91
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-01-19 17:27:57 -0800

    feat: add Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, :body, presence: true
+
+  validates :title, length: { in: 3..40 }
+  validates :body, length: { minimum: 4 }
 end
