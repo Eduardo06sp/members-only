a51944f4b31d97c75726f22a9c2fe005b2799932
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-01-19 18:47:36 -0800

    feat: configure router to use generated registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   root 'posts#index'
 
   resources :posts, only: [:new, :create, :index]
