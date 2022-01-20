c261fb4f1094e44f3827fe5adf4264d720be8eca
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-01-19 17:17:40 -0800

    feat: add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, :email, :password, presence: true
+  validates :username, :email, uniqueness: true
+
+  validates :username, :email, uniqueness: true
+  validates :username, length: { in: 4..12 }
+  validates :password, length: { in: 6..30 }
 end
