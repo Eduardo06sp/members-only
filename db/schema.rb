59a7872220b44b2e5cb45b22bcc6c83333beafd3
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-01-19 17:35:02 -0800

    feat: remove author column from posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_075916) do
+ActiveRecord::Schema.define(version: 2022_01_20_013331) do
 
   create_table "posts", force: :cascade do |t|
-    t.string "author"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
