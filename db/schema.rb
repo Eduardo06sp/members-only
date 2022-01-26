546606505e2cea1c90afeae1c27215e0ad088e97
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-01-26 15:34:38 -0800

    refactor: convert database to postgresql db

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2022_01_20_013331) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
